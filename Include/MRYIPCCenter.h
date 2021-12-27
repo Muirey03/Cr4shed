1bc2cf921378a34137747418278c381503b0b72c
Tanner Bennett <tannerbennett@me.com>
2021-12-27 02:33:24 -0600

    Update MRYIPCCenter.h
    withArguments: args should be `id`

--- Include/MRYIPCCenter.h
+++ Include/MRYIPCCenter.h
@@ -3,11 +3,11 @@
 +(instancetype)centerNamed:(NSString*)name;
 -(void)addTarget:(id)target action:(SEL)action;
 //asynchronously call a void method
--(void)callExternalVoidMethod:(SEL)method withArguments:(NSDictionary*)args;
+-(void)callExternalVoidMethod:(SEL)method withArguments:(id)args;
 //synchronously call a method and recieve the return value
--(id)callExternalMethod:(SEL)method withArguments:(NSDictionary*)args;
+-(id)callExternalMethod:(SEL)method withArguments:(id)args;
 //asynchronously call a method and receive the return value in the completion handler
--(void)callExternalMethod:(SEL)method withArguments:(NSDictionary*)args completion:(void(^)(id))completionHandler;
+-(void)callExternalMethod:(SEL)method withArguments:(id)args completion:(void(^)(id))completionHandler;
 
 //deprecated
 -(void)registerMethod:(SEL)selector withTarget:(id)target;
