816b94621733f4b36bb2648c3cfeec5420c5e2a4
Lightmann <lightmanns11@gmail.com>
2023-07-08 18:53:11 -0400

    Swap NSTask for posix_spawn (needed for rootless atm)

--- cr4shedgui/dpkgutils.h
+++ cr4shedgui/dpkgutils.h
@@ -1,13 +1,5 @@
 #import <Foundation/Foundation.h>
 
-@interface NSTask : NSObject
-@property (copy) NSURL* executableURL;
-@property (copy) NSArray* arguments;
-@property (retain) id standardOutput;
--(void)launch;
--(void)waitUntilExit;
-@end
-
 NSString* outputOfCommand(NSString* cmd, NSArray<NSString*>* args);
 NSString* packageForFile(NSString* file);
 NSString* controlFieldForPackage(NSString* package, NSString* field);
--- cr4shedgui/dpkgutils.m
+++ cr4shedgui/dpkgutils.m
@@ -1,21 +1,70 @@
 #import "dpkgutils.h"
 #import <rootless.h>
+#import <spawn.h>
+
+extern char **environ;
+
+// https://stackoverflow.com/a/13065080
+char** getArray(NSArray *arr)
+{
+	unsigned count = [arr count];
+	char **array = (char **)malloc((count + 1) * sizeof(char*));
+	for (unsigned i = 0; i < count; i++)
+	{
+		array[i] = strdup([[arr objectAtIndex:i] UTF8String]);
+	}
+	array[count] = NULL;
+	return array;
+}
+
+void freeArray(char **array)
+{
+	if (array != NULL)
+	{
+		for (unsigned index = 0; array[index] != NULL; index++)
+		{
+			free(array[index]);
+		}
+		free(array);
+	}
+}
 
 NSString* outputOfCommand(NSString* cmd, NSArray<NSString*>* args)
 {
-	NSTask* task = [NSTask new];
-	task.executableURL = [NSURL fileURLWithPath:cmd];
-	task.arguments = args;
+	// Sanity check
+	if (strstr(getenv("PATH"), "/var/jb") == NULL) {
+		// https://github.com/opa334/Dopamine/blob/1595dbf05561e55aa36e8dd39a77ebe2a5dd00c1/Packages/Fugu15KernelExploit/Sources/Fugu15KernelExploit/oobPCI.swift#L252
+		setenv("PATH", "/sbin:/bin:/usr/sbin:/usr/bin:/var/jb/sbin:/var/jb/bin:/var/jb/usr/sbin:/var/jb/usr/bin", 1);
+	}
 
-	NSPipe* p = [NSPipe pipe];
-	task.standardOutput = p;
+	int out[2];
+	if (pipe(out) != 0)
+		return nil;
 
-	[task launch];
-	[task waitUntilExit];
+	posix_spawn_file_actions_t action;
+	posix_spawn_file_actions_init(&action);
+	posix_spawn_file_actions_adddup2(&action, out[1], STDOUT_FILENO);
+	posix_spawn_file_actions_addclose(&action, out[0]);
 
-	NSFileHandle* handle = [p fileHandleForReading];
-	NSData* data = [handle readDataToEndOfFile];
-	return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+	pid_t pid;
+	posix_spawn(&pid, [cmd UTF8String], &action, NULL, getArray(args), environ);
+	close(out[1]);
+	waitpid(pid, NULL, 0);
+
+	const size_t buffSize = PIPE_BUF;
+	char buff[buffSize];
+	ssize_t rBytes;
+	while (( rBytes = read(out[0], buff, buffSize )) > 0)
+	{
+		rBytes = read(out[0], buff, buffSize);
+	}
+
+	if (rBytes == -1)
+	   return nil;
+
+	posix_spawn_file_actions_destroy(&action);
+
+	return [NSString stringWithUTF8String:buff];
 }
 
 NSString* packageForFile(NSString* file)
